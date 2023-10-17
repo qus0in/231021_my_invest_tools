8a8b2e32d2a1ee162ab382aeada0b36980fae9ce
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-10-17 12:48:11 +0845

    from .dynamo_db import DynamoDB

--- ko_etf_tools/__init__.py
+++ ko_etf_tools/__init__.py
@@ -1,3 +1,4 @@
 from .ko_etf import KoETF
+from .dynamo_db import DynamoDB
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
---
+++ ko_etf_tools/dynamo_db.py
@@ -0,0 +1,21 @@
+import requests
+
+class DynamoDB:
+    def __init__(self, API_URL, API_KEY, TABLE_NAME):
+        self.API_URL = API_URL
+        self.API_KEY = API_KEY
+        self.TABLE_NAME = TABLE_NAME
+
+    def put_item(self, item: dict):
+        data = {'TableName': self.TABLE_NAME, 'Item': item}
+        headers = {'x-api-key': self.API_KEY}
+        response = requests.post(self.API_URL, json=data, headers=headers)
+        return response
+    
+    def query(self, expr:str, attr_val:dict):
+        data = {'TableName': self.TABLE_NAME,
+                'KeyConditionExpression': expr,
+                'ExpressionAttributeValues': attr_val}
+        headers = {'x-api-key': self.API_KEY}
+        response = requests.post(self.API_URL + '/query', json=data, headers=headers)
+        return response
