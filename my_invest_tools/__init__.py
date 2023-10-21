0448864822a77ee4ff052ea6ea7a250eb02fe05e
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-10-21 19:25:24 +0845

    from .kis_account import KISAccount

--- my_invest_tools/__init__.py
+++ my_invest_tools/__init__.py
@@ -6,4 +6,6 @@ from .invest_screener import InvestScreener
 from .invest_market import InvestMarket
 from .invest_analysis import InvestAnalysis
 
+from .kis_account import KISAccount
+
 __version__ = '0.0.7'
